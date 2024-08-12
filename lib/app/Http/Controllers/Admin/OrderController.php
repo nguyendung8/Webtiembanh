a9ba5f9d3c16bffe33fbbfe136b908f651e76f68
Nguyen Manh Dung <kiaisoft.dung.nguyen1@gmail.com>
2024-08-12 22:18:50 +0700

    init

--- lib/app/Http/Controllers/Admin/OrderController.php
+++ lib/app/Http/Controllers/Admin/OrderController.php
@@ -47,7 +47,7 @@ class OrderController extends Controller
 
             $message->to($email, $name);
 
-            $message->subject('Thông báo đơn hàng của bạn đã được xác nhận tại Bigsize Thiên An');
+            $message->subject('Thông báo đơn hàng của bạn đã được xác nhận tại Q Shoes');
 
         });
         return redirect()->intended('admin/order')->with('success', 'Xác nhận đơn hàng thành công!');
@@ -67,7 +67,7 @@ class OrderController extends Controller
 
             $message->to($email, $name);
 
-            $message->subject('Thông báo đơn hàng của bạn đang được vận chuyển từ Bigsize Thiên An');
+            $message->subject('Thông báo đơn hàng của bạn đang được vận chuyển từ Q Shoes');
 
         });
         return redirect()->intended('admin/order')->with('success', 'Cập nhật đơn hàng thành công!');;
